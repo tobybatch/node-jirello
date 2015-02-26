2446e74000cec46debdee1e40764c655ce0f735e
Toby Batch <tobias@neontribe.co.uk>
2015-02-26 16:15:06 +0000

    Added example config file

---
+++ config.dist.js
@@ -0,0 +1,10 @@
+{
+    jira: {
+        user: 'nt_user',
+        pass: 'password'
+    },
+    trello: {
+        key: 'xxxxxx_public_key_xxxxxxxxxxxxxx',
+        token: 'xxxxxxx_generated_application_token_xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    }
+}
